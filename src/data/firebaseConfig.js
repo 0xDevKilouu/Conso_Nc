16df4ad8f3382d1ca7aded6022082732cc3fceba
0xDevKilouu <138083927+0xDevKilouu@users.noreply.github.com>
2024-07-22 02:02:14 +0000

    up

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -3,13 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyCenXl0HoRahcnsDZuHe_fw3dli97shei0",
+    authDomain: "consonc-26043.firebaseapp.com",
+    projectId: "consonc-26043",
+    storageBucket: "consonc-26043.appspot.com",
+    messagingSenderId: "347349604983",
+    appId: "1:347349604983:web:8300836901a9bf62768472",
+    measurementId: "G-BY69W74409"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
