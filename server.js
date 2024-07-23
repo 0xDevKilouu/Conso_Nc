be223ec4b7abb099e495ffed673062a1da897198
0xDevKilouu <138083927+0xDevKilouu@users.noreply.github.com>
2024-07-23 23:29:43 +0000

    d

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -31,6 +31,7 @@ app.get('/secure-data', async (req, res) => {
     const data = await admin.firestore().collection('secure-collection').get();
     res.send(data.docs.map(doc => doc.data()));
   } catch (error) {
+    console.error('Error retrieving data:', error);
     res.status(500).send('Error retrieving data');
   }
 });
