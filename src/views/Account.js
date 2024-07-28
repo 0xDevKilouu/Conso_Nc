aa18c9538f69aefc1036e8ae35ef6aec15656887
0xDevKilouu <138083927+0xDevKilouu@users.noreply.github.com>
2024-07-28 23:43:40 +0000

    de

--- src/views/Account.js
+++ src/views/Account.js
@@ -56,8 +56,9 @@ const handleAuthStateChange = () => {
 document.addEventListener('DOMContentLoaded', () => {
   getRedirectResult(auth)
     .then((result) => {
-      if (result && result.user) {
-        console.log('Utilisateur connecté après redirection:', result.user);
+      if (result.user) {
+        console.log('Utilisateur connecté:', result.user);
+        // Traiter l'utilisateur connecté
         const content = document.getElementById('content');
         if (window.location.hash.substring(1) === 'account') {
           content.innerHTML = renderAccountPage(result.user);
