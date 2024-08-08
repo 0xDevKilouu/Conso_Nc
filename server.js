ad77b7941aa78c5c0f4be65e121fdd14499bccbe
0xDevKilouu <138083927+0xDevKilouu@users.noreply.github.com>
2024-08-08 20:37:58 +0000

    update CORS

--- server.js
+++ server.js
@@ -7,18 +7,6 @@ const bodyParser = require('body-parser');
 
 dotenv.config();
 
-console.log('Environment Variables:');
-console.log('FIREBASE_TYPE:', process.env.FIREBASE_TYPE);
-console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
-console.log('FIREBASE_PRIVATE_KEY_ID:', process.env.FIREBASE_PRIVATE_KEY_ID);
-console.log('FIREBASE_PRIVATE_KEY:', process.env.FIREBASE_PRIVATE_KEY);
-console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
-console.log('FIREBASE_CLIENT_ID:', process.env.FIREBASE_CLIENT_ID);
-console.log('FIREBASE_AUTH_URI:', process.env.FIREBASE_AUTH_URI);
-console.log('FIREBASE_TOKEN_URI:', process.env.FIREBASE_TOKEN_URI);
-console.log('FIREBASE_AUTH_PROVIDER_CERT_URL:', process.env.FIREBASE_AUTH_PROVIDER_CERT_URL);
-console.log('FIREBASE_CLIENT_CERT_URL:', process.env.FIREBASE_CLIENT_CERT_URL);
-
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
@@ -47,17 +35,14 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log('Request origin:', origin);
-    if (allowedOrigins.some((allowedOrigin) => 
-      typeof allowedOrigin === 'string' ? allowedOrigin === origin : allowedOrigin.test(origin)
-    ) || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      console.error('Not allowed by CORS');
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
   preflightContinue: false,
   optionsSuccessStatus: 204
 };
@@ -67,26 +52,24 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.post('/github-webhook', (req, res) => {
-  console.log('Received GitHub webhook:', req.body);
   res.status(200).send('Webhook received');
 });
 
 app.get('/secure-data', async (req, res) => {
-  console.log('Received request for /secure-data');
   try {
     const data = await admin.firestore().collection('secure-collection').get();
-    if (!data.empty) {
-      const jsonData = data.docs.map(doc => doc.data());
-      console.log('Data retrieved successfully:', jsonData);  
-      res.json(jsonData);
-    } else {
-      console.log('No data found');  
-      res.json([]);
-    }
+    res.json(data.empty ? [] : data.docs.map(doc => doc.data()));
   } catch (error) {
-    console.error('Error retrieving data:', error.message);
     res.status(500).json({ error: 'Error retrieving data' });
   }
+});
+
+// Add CORS headers to all responses
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
 });
 
 app.get('*', (req, res, next) => {
@@ -95,10 +78,8 @@ app.get('*', (req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-  console.log('Received request for', req.originalUrl);
   res.sendFile(path.join(__dirname, 'dist', 'index.html'), err => {
     if (err) {
-      console.error('Error sending index.html:', err);
       res.status(500).send(err);
     }
   });
@@ -106,7 +87,6 @@ app.get('*', (req, res) => {
 
 // Global error handler
 app.use((err, req, res, next) => {
-  console.error('Global error handler:', err);
   res.status(500).send('Something went wrong!');
 });
 
