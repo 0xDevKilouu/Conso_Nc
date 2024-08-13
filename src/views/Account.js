7d28197451db727afe9e5f0bc0323c4de4af4e51
0xDevKilouu <138083927+0xDevKilouu@users.noreply.github.com>
2024-08-13 05:40:56 +0000

    update

--- src/views/Account.js
+++ src/views/Account.js
@@ -1,4 +1,4 @@
-import { auth, ui, uiConfig } from '../firebaseConfig';
+import { auth } from '../firebaseConfig';
 import { getRedirectResult } from "firebase/auth";
 
 let isUpdateFormVisible = false;
@@ -24,16 +24,10 @@ const renderUserInfo = (user) => `
   </div>
 `;
 
-const renderAuthUI = () => `
-  <div id="account-container">
-    <div id="firebaseui-auth-container"></div>
-  </div>
-`;
-
 const renderAccountPage = (user) => `
   <div id="account">
     <h2>${user ? 'Compte' : 'Connexion'}</h2>
-    ${user ? renderUserInfo(user) : renderAuthUI()}
+    ${user ? renderUserInfo(user) : `<div id="firebaseui-auth-container"></div>`}
   </div>
 `;
 
@@ -84,51 +78,15 @@ const attachEventListeners = () => {
   }
 };
 
-// Fonction pour remplacer le texte de l'interface Firebase UI par du texte en français
-const replaceFirebaseUIText = () => {
-  const termsElement = document.querySelector('.firebaseui-card-footer');
-  if (termsElement) {
-    termsElement.innerHTML = `
-      En continuant, vous indiquez que vous acceptez nos 
-      <a href="<your-terms-of-service-url>" target="_blank">Conditions d'utilisation</a> 
-      et notre 
-      <a href="<your-privacy-policy-url>" target="_blank">Politique de confidentialité</a>.
-    `;
-  }
-
-  document.querySelectorAll('.firebaseui-id-submit').forEach(button => {
-    if (button.innerText.toLowerCase() === 'next') {
-      button.innerText = 'Suivant';
-    }
-    if (button.innerText.toLowerCase() === 'cancel') {
-      button.innerText = 'Annuler';
-    }
-    if (button.innerText.toLowerCase() === 'back') {
-      button.innerText = 'Retour';
-    }
-  });
-
-  document.querySelectorAll('.firebaseui-id-secondary-link').forEach(link => {
-    if (link.innerText.toLowerCase().includes('trouble getting email')) {
-      link.innerText = 'Problème pour recevoir l\'email ?';
-    }
-  });
-
-  document.querySelectorAll('.firebaseui-id-page-sign-in').forEach(signInText => {
-    if (signInText.innerText.toLowerCase().includes('sign-in email sent')) {
-      signInText.innerText = 'Email de connexion envoyé';
-    }
-  });
-};
-
 const handleAuthStateChange = () => {
   auth.onAuthStateChanged((user) => {
     if (window.location.hash.substring(1) === 'account') {
       document.getElementById('content').innerHTML = renderAccountPage(user);
       attachEventListeners();
       if (!user) {
-        ui.start('#firebaseui-auth-container', uiConfig);
-        replaceFirebaseUIText(); // Appeler la fonction ici après le rendu de l'interface
+        import('./firebaseUI').then(({ startFirebaseUI }) => {
+          startFirebaseUI();  // Démarre Firebase UI après avoir rendu la page
+        });
       }
     }
   });
